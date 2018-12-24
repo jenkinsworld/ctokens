78a29a718723d3550098acafadec57d70b77a6d9
chandrasekhar <gchandrasekhar.cm@gmail.com>
2018-12-24 14:28:53 +0530

    JIRA123

--- git.sh
+++ git.sh
@@ -11,6 +11,6 @@ else
   echo "commited the changes successfully"
   VERSION=`git describe --tags --abbrev=0 | awk -F. '{$NF+=1; OFS="."; print $0}'`
   git tag -a `echo $VERSION | awk -F' ' 'BEGIN{OFS=".";} {print $1,$2,$3;}'` -m "new release"
-  git push origin `echo $VERSION | awk -F' ' 'BEGIN{OFS=".";} {print $1,$2,$3;}'
+  git push origin `echo $VERSION | awk -F' ' 'BEGIN{OFS=".";} {print $1,$2,$3;}'`
 fi
 
