a1a086c5be4bc0104a15f5c233146dec00e08d24
chandrasekhar <gchandrasekhar.cm@gmail.com>
2018-12-27 17:34:22 +0530

    Update ping_servers.sh
    modified

--- ping_servers.sh
+++ ping_servers.sh
@@ -1,20 +1,27 @@
 #!bin/bash
+rm -rf failed.txt
 
-for every_ip in `cat plain.txt`;
+for every_ip in `cat ip.txt`;
 #for every_ip in "10.30.30.200";
 do
-    ping "$every_ip" -q -c 1 -t 2
-    retping=$?
-    nc -z "$every_ip" 3389
-    retnc=$?
-        echo $retping
-        echo $retnc
-   if [ $retping -ne 0  ] || [ $retnc -ne 0  ]
+   #ping -q -c 1 -t 2 "$every_ip"
+   ping -q -c 1 -t 2 "$every_ip" 2>&1 > /dev/null
+   retping=$?
+   retnc=`nc -z -w 3 "$every_ip" 3389 &> /dev/null && echo "Online" || echo "Offline"`
+   #nc -z "$every_ip" 3389
+   #retnc=$?
+   #echo $retping
+   #echo $retnc
+   #set -x
+   if [ "$retping" -ne "0"  ] || [ "$retnc" != "Online"  ]
+   #if [ $retping -ne 0  ]
    then
-    echo "=================== ping failed ==============="
-        echo $every_ip >> failed.txt
-        var=$((var + 1))
+   #set +x
+    echo "=================== ping failed for $every_ip ==============="
+    echo $every_ip >> failed.txt
+    #var=$((var + 1))
    fi
+   #set +x
 done
-echo $var
+#echo $var
 cat failed.txt
